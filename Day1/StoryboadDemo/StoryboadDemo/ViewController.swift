19e40f41473a7209b8fc77082536af68cc13ac82
Alexiuce <alexiuce@qq.com>
2017-08-12 14:09:17 +0800

    add storyboard

---
+++ Day1/StoryboadDemo/StoryboadDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StoryboadDemo
+//
+//  Created by Alexcai on 2017/8/12.
+//  Copyright © 2017年 Alexcai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

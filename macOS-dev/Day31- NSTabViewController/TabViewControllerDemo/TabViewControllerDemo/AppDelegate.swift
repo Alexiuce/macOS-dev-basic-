d3e2034b2c6e3566d75fe657c2ae7203bff664c2
Alexiuce <alexiuce@qq.com>
2018-09-09 09:24:36 +0800

    feat(TabViewController): 初始化
    Day31 添加NSTableViewController 示例工程

---
+++ macOS-dev/Day31- NSTabViewController/TabViewControllerDemo/TabViewControllerDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TabViewControllerDemo
+//
+//  Created by Alexcai on 2018/9/9.
+//  Copyright © 2018年 dongjiu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS-dev/Day31- NSTabViewController/TabViewControllerDemo/TabViewControllerDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TabViewControllerDemo
+//
+//  Created by Alexcai on 2018/9/9.
+//  Copyright © 2018年 dongjiu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

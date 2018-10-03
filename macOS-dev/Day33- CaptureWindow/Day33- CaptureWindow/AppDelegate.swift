6c713838152d0defa3c0f0cc599f38f299cd7190
Alexiuce <alexiuce@qq.com>
2018-10-03 13:50:36 +0800

    feat(Day33): 添加截屏示例

---
+++ macOS-dev/Day33- CaptureWindow/Day33- CaptureWindow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Day33- CaptureWindow
+//
+//  Created by Alexcai on 2018/10/3.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS-dev/Day33- CaptureWindow/Day33- CaptureWindow/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Day33- CaptureWindow
+//
+//  Created by Alexcai on 2018/10/3.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+

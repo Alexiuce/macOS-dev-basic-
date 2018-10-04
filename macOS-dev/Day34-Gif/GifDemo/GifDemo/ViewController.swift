8f4c8ce75379c5d0421194a50abd707f0e2a8e92
Alexiuce <alexiuce@qq.com>
2018-10-04 17:34:59 +0800

    refactor(Day34): NSImage 代理
    代理方法不执行

--- macOS-dev/Day34-Gif/GifDemo/GifDemo/ViewController.swift
+++ macOS-dev/Day34-Gif/GifDemo/GifDemo/ViewController.swift
@@ -10,9 +10,17 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var imageView: NSImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let image = NSImage(size: NSSize(width: 110, height: 84))
+        image.delegate = self
+        let result =  image.setName(NSImage.Name.init("timg.gif"))
+        print("\(result)")
+        imageView.image = image
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -23,5 +31,20 @@ class ViewController: NSViewController {
     }
 
 
+    
+    override func mouseDown(with event: NSEvent) {
+        imageView.animates = !imageView.animates
+    }
 }
 
+
+
+extension ViewController: NSImageDelegate{
+    func image(_ image: NSImage, didLoadRepresentationHeader rep: NSImageRep) {
+        print("did load represent header")
+    }
+    
+    func image(_ image: NSImage, willLoadRepresentation rep: NSImageRep) {
+        print("will load represnet")
+    }
+}

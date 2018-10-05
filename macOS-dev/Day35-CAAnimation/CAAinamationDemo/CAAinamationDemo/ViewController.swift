e9b8e25be5a88c06b837272a6286b4a5389ecbe1
Alexiuce <alexiuce@qq.com>
2018-10-05 19:45:13 +0800

    refactor(Day35): CAShapeLayer
    path

--- macOS-dev/Day35-CAAnimation/CAAinamationDemo/CAAinamationDemo/ViewController.swift
+++ macOS-dev/Day35-CAAnimation/CAAinamationDemo/CAAinamationDemo/ViewController.swift
@@ -33,6 +33,39 @@ class ViewController: NSViewController {
         
         view.layer?.addSublayer(shaperLayer)
         sLayer = shaperLayer
+        
+        let startPoint = CGPoint(x: 50, y: 300)
+        let endPoint = CGPoint(x:300,y:300)
+        let controlPoint = CGPoint(x:170, y:400)
+        
+        let layer1 = CALayer()
+        layer1.frame = CGRect(x:startPoint.x, y:startPoint.y,width: 5,height: 5)
+        layer1.backgroundColor = NSColor.red.cgColor
+        
+        let layer2 = CALayer()
+        layer2.frame = CGRect(x:endPoint.x, y:endPoint.y, width:5,height: 5)
+        layer2.backgroundColor = NSColor.red.cgColor
+        
+        let layer3 = CALayer()
+        layer3.frame = CGRect(x:controlPoint.x, y:controlPoint.y,width: 5, height:5)
+        layer3.backgroundColor = NSColor.red.cgColor
+        
+        let path = NSBezierPath()
+        let layer = CAShapeLayer()
+        
+        path.move(to: startPoint)
+        path.curve(to: endPoint, controlPoint1: controlPoint, controlPoint2: controlPoint)
+
+        
+        layer.path = path.cgPath
+        layer.fillColor = NSColor.clear.cgColor
+        layer.strokeColor = NSColor.black.cgColor
+        
+        view.layer?.addSublayer(layer)
+        view.layer?.addSublayer(layer1)
+        view.layer?.addSublayer(layer2)
+        view.layer?.addSublayer(layer3)
+        sLayer = layer
       
         
     }
@@ -65,22 +98,34 @@ extension ViewController{
     func strokeEndAnmi()  {
     
         
-        let shapePath = NSBezierPath(rect: CGRect(x: 10, y: 10, width: 90, height: 10))
-        sLayer.path = shapePath.cgPath
-        sLayer.fillColor = NSColor.orange.cgColor
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.duration = 2
+        sLayer.add(animation, forKey: "")
         
+//        let animation1 = CABasicAnimation(keyPath: "strokeStart")
+//        animation1.fromValue = 0
+//        animation1.toValue = 1
+//        animation1.duration = 2
+//        sLayer.add(animation1, forKey: "")
         
-        let moveAnim = CABasicAnimation(keyPath: "strokeEnd")
-
-
-        moveAnim.fromValue = NSNumber.init(value: 0)
-        moveAnim.toValue = NSNumber.init(value: 0.5)
-        moveAnim.duration = 6
-
-        moveAnim.fillMode = .forwards
-        moveAnim.isRemovedOnCompletion = false
-
-        sLayer.add(moveAnim, forKey: nil)
+//        let shapePath = NSBezierPath(rect: CGRect(x: 10, y: 10, width: 90, height: 10))
+//        sLayer.path = shapePath.cgPath
+//        sLayer.fillColor = NSColor.orange.cgColor
+//
+//
+//        let moveAnim = CABasicAnimation(keyPath: "strokeEnd")
+//
+//
+//        moveAnim.fromValue = 0
+//        moveAnim.toValue = 1
+//        moveAnim.duration = 6
+//
+//        moveAnim.fillMode = .forwards
+//        moveAnim.isRemovedOnCompletion = false
+//
+//        sLayer.add(moveAnim, forKey: nil)
         
     }
 }

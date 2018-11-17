f9bf1b71dace782977230dad40da775c65307a9d
alexiuce <alexiuce@qq.com>
2018-11-17 19:36:09 +0800

    refactor(Day36-DarkModeAdapter): 带语意的NSColor

--- macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/ViewController.swift
+++ macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/ViewController.swift
@@ -17,15 +17,21 @@ class ViewController: NSViewController {
         /** 1. 颜色硬编码设置视图背景色 : 这种情况下,无论是light 或者dark 模式,颜色都是固定的值,不会根据主题进行适配 */
 //        adaptedView.layer?.backgroundColor = NSColor.red.cgColor
         
-        /**2. 使用Asset 中的color 进行light /dark 之间的颜色适配: 切换light和dark时,需要重新开启应用 */
-        adaptedView.layer?.backgroundColor = NSColor(named: "Color")?.cgColor
-        // Do any additional setup after loading the view.
+        /** 2. 使用Asset 中的color 进行light /dark 之间的颜色适配: 切换light和dark时,需要重新开启应用 */
+//        adaptedView.layer?.backgroundColor = NSColor(named: "Color")?.cgColor
+        
+        /** 3. 使用带有语意的NSColor */
+//        adaptedView.layer?.backgroundColor = NSColor.labelColor.cgColor;
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+         adaptedView.layer?.backgroundColor = NSColor.labelColor.cgColor;
     }
 
 

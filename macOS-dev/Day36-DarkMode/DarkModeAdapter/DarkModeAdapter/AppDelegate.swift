bbfb264e072658b4f42256fdfa622a3929553b33
alexiuce <alexiuce@qq.com>
2018-11-17 13:08:51 +0800

    feat(Day36-DarkMode): 适配macOS10.14 暗黑模式

---
+++ macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DarkModeAdapter
+//
+//  Created by Alexcai on 2018/11/17.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DarkModeAdapter
+//
+//  Created by Alexcai on 2018/11/17.
+//  Copyright © 2018 dongjiu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

6f1668cbf7426a5d846d3f4e1a87f51e4003470a
alexiuce <alexiuce@qq.com>
2018-11-17 18:59:11 +0800

    refactor(Day36-DarkModeAdapter): 添加AssetsColor 进行light/Dark 适配

--- macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/ViewController.swift
+++ macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/ViewController.swift
@@ -10,9 +10,15 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var adaptedView: NSView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        /** 1. 颜色硬编码设置视图背景色 : 这种情况下,无论是light 或者dark 模式,颜色都是固定的值,不会根据主题进行适配 */
+//        adaptedView.layer?.backgroundColor = NSColor.red.cgColor
+        
+        /**2. 使用Asset 中的color 进行light /dark 之间的颜色适配: 切换light和dark时,需要重新开启应用 */
+        adaptedView.layer?.backgroundColor = NSColor(named: "Color")?.cgColor
         // Do any additional setup after loading the view.
     }
 

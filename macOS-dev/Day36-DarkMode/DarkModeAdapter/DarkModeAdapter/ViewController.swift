c0ec1448f837a144ede3d47363be21d298124868
alexiuce <alexiuce@qq.com>
2018-11-18 11:45:26 +0800

    refactor(Day36-DarkModeAdapter): 添加图片的适配素材

--- macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/ViewController.swift
+++ macOS-dev/Day36-DarkMode/DarkModeAdapter/DarkModeAdapter/ViewController.swift
@@ -11,6 +11,10 @@ import Cocoa
 class ViewController: NSViewController {
 
     @IBOutlet weak var adaptedView: NSView!
+    
+    
+    @IBOutlet weak var starImageView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,20 +22,19 @@ class ViewController: NSViewController {
 //        adaptedView.layer?.backgroundColor = NSColor.red.cgColor
         
         /** 2. 使用Asset 中的color 进行light /dark 之间的颜色适配: 切换light和dark时,需要重新开启应用 */
-//        adaptedView.layer?.backgroundColor = NSColor(named: "Color")?.cgColor
+        adaptedView.layer?.backgroundColor = NSColor(named: "Color")?.cgColor
         
         /** 3. 使用带有语意的NSColor */
 //        adaptedView.layer?.backgroundColor = NSColor.labelColor.cgColor;
+        
+        
+        starImageView.image = NSImage(named: "star")
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
-    }
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
-         adaptedView.layer?.backgroundColor = NSColor.labelColor.cgColor;
     }
 
 
